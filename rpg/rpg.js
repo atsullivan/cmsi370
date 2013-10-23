75d806d42956181aa28e32cdb8a66cdc7a5c4bea
atsullivan <asulli17@lion.lmu.edu>
2013-10-22 22:57:39 -0700

    Update rpg.js

--- rpg/rpg.js
+++ rpg/rpg.js
@@ -1,11 +1,37 @@
 // Big things have small beginnings...
 $(function () {
+    $("#delete-button").popover({ placement: 'left' });
+    
+    var characterRowTemplate = '<tr>' +
+          '<td><input type="checkbox" value="0"></td>' +
+          '<td><a href="character.html#11111"></a></td>' +
+          '<td></td>' +
+          '<td></td>' +
+          '<td></td>' +
+          '<td></td>' +
+        '</tr>';
 
-    $("#confirm-delete-button").click(function () {
-        console.log("Delete confirmed!!!!!");
-
-        // Now we dismiss the dialog.
-        $('#deleteModal').modal('hide');
+     $("#confirm-create-button").click(function () {
+        console.log("Create confirmed!");
+        window.location = "character.html#" + $("#createModal input.name").val() +
+            "#" + $("#createModal input.level").val() + "#" + $("#createModal select.gender").val() +
+            "#" + $("#createModal input.money").val();
     });
+    
+    $.getJSON(
+        "http://lmu-diabolical.appspot.com/characters",
+        function (characters) {
+            // Do something with the character list.
+            characters.forEach(function (character) {
+                var $characterRow = $(characterRowTemplate);
+                $characterRow.find("td:nth-child(2) > a")
+                    .attr({ href: "character.html#" + character.id })
+                    .text(character.name);
+                $characterRow.find("td:nth-child(3)").text(character.classType);
+                $characterRow.find("td:nth-child(4)").text(character.gender.substr(0, 1));
+                $("#character-table > tbody").append($characterRow);
+            });
+        }
+    );
 
 });
